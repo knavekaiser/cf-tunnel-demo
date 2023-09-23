49b23321a7d8fc485fad178e807adf959853d7ca
Naeem Ahmad <naeem.ahmad.9m@gmail.com>
2023-09-23 16:30:12 +0600

    update

--- src/index.js
+++ src/index.js
@@ -1,32 +1,31 @@
 // import { Client as MySqlClient } from './driver/mysql';
 // const mysql = new MySqlClient();
 
+const mysql = require('mysql2/promise');
 import { Client as PgClient } from 'pg';
 // import { Client } from '@neondatabase/serverless';
 
-// const getDataFromSql = async () => {
-// 	const sqlCred = {
-// 		hostname: 'sql12.freesqldatabase.com',
-// 		port: '3306',
-// 		db: 'sql12648414',
-// 		username: 'sql12648414',
-// 		password: 'dt8iKEcGu3',
-// 	};
+const getDataFromSql = async () => {
+	const sqlCred = {
+		host: 'sql12.freesqldatabase.com',
+		// port: '3306',
+		database: 'sql12648414',
+		user: 'sql12648414',
+		password: 'dt8iKEcGu3',
+	};
 
-// 	const client = new mysql(sqlCred);
-// 	await client.connect();
+	const connection = await mysql.createConnection(sqlCred);
 
-// 	const result = await client.query({
-// 		text: 'SELECT * FROM users',
-// 	});
-// 	return { database: 'postgres', data: result.rows };
-// };
+	const result = await connection.query('SELECT * FROM users');
+
+	return { database: 'mysql', data: result, message: err.message };
+};
 
 const getDataFromPostgres = async () => {
 	const postgresCred = {
 		host: 'john.db.elephantsql.com',
 		port: '5432',
-		db: 'hzldgkxs',
+		database: 'hzldgkxs',
 		user: 'hzldgkxs',
 		password: 'esI4A3zLYEI7S8wUaLuD57ldPHMb5q1n',
 	};
